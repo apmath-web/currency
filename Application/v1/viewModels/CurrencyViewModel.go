745f89b4a55bdc89dc0d5105e49b5be9e26d9a28
sashachernyshova <st056039@student.spbu.ru>
2019-05-17 18:33:24 +0300

    add serialising to and from json

--- Application/v1/viewModels/CurrencyViewModel.go
+++ Application/v1/viewModels/CurrencyViewModel.go
@@ -1,5 +1,7 @@
 package viewModels
 
+import "encoding/json"
+
 type CurrencyViewModel struct {
 	Amount          int    `json:"amount"`
 	CurrentCurrency string `json:"currentCurrency"`
@@ -17,4 +19,18 @@ func (c *CurrencyViewModel) GetCurrentCurrency() int {
 
 func (c *CurrencyViewModel) GetWantedCurrency() int {
 	return c.Amount
+}
+
+func (c *CurrencyViewModel) MarshalJSON() (b []byte, e error) {
+	return json.Marshal(map[string]interface{}{
+		"amount":   c.Amount,
+		"currency": c.CurrentCurrency,
+	})
+}
+
+func (c *CurrencyViewModel) UnmarshalJSON(b []byte) error {
+	if err := json.Unmarshal(b, &c); err != nil {
+		return err
+	}
+	return nil
 }
