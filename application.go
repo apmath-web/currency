0ade878587778bfa042e24b15f7df3be35dba857
sashachernyshova <st056039@student.spbu.ru>
2019-05-17 18:19:42 +0300

    create v1 api folder

--- application.go
+++ application.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/apmath-web/currency/Application/routing"
+	"github.com/apmath-web/currency/Application/v1"
 	"log"
 )
 
 func main() {
-	router := routing.GenRouter()
+	router := v1.GenRouter()
 	err := router.Run("0.0.0.0:8080")
 	if err != nil {
 		log.Fatal(err.Error())
