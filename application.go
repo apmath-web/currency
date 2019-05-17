0722162cb2e797505a6cf522aaacbae8c4569209
sashachernyshova <st056039@student.spbu.ru>
2019-05-17 18:42:38 +0300

    fix bug

--- application.go
+++ application.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/apmath-web/currency/Application/v1"
+	"github.com/apmath-web/currency/Application/v1/routing"
 	"log"
 )
 
 func main() {
-	router := v1.GenRouter()
+	router := routing.GenRouter()
 	err := router.Run("0.0.0.0:8080")
 	if err != nil {
 		log.Fatal(err.Error())
