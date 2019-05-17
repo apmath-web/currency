b2fd502dbafbc07f646538b97c60eb90931fa44b
sashachernyshova <st056039@student.spbu.ru>
2019-05-17 18:31:20 +0300

    add a struct of view model

--- Application/v1/viewModels/CurrencyViewModel.go
+++ Application/v1/viewModels/CurrencyViewModel.go
@@ -1,1 +1,8 @@
 package viewModels
+
+type CurrencyViewModel struct {
+	Amount          int    `json:"amount"`
+	CurrentCurrency string `json:"currentCurrency"`
+	WantedCurrency  string `json:"wantedCurrency"`
+	validation      interface{}
+}
