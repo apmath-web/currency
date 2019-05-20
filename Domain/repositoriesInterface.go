0c7cc70c068633ba57139f65afd862582293c910
sashachernyshova <st056039@student.spbu.ru>
2019-05-20 14:25:06 +0300

    add currency rate repo

--- Domain/repositoriesInterface.go
+++ Domain/repositoriesInterface.go
@@ -1,1 +1,7 @@
 package Domain
+
+type CurrencyRateRepositoryInterface interface {
+	GetCurrencyRate(curCurrency Currency, wanCurrency Currency) CurrencyRate
+	SetCurrencyRate(rate CurrencyRate) error
+	UpdateCurrencyRate(rate CurrencyRate) error
+}
