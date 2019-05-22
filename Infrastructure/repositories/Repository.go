89f30f9f6e3aee41d6b727487af685bfb0021e29
Anatoly Efimov <anatolyefimov@yandex-team.ru>
2019-05-23 01:39:23 +0300

    Релизция репозитория

--- Infrastructure/repositories/Repository.go
+++ Infrastructure/repositories/Repository.go
@@ -1,1 +1,23 @@
 package repositories
+
+import (
+	"github.com/apmath-web/currency/Domain"
+)
+
+type Repository struct {
+	currentRates Domain.ChangeTable
+}
+
+func (repository Repository) SetAllTable(table Domain.ChangeTable) error {
+	repository.currentRates = table
+	return nil
+}
+
+func (repository Repository) GetAllTable() Domain.ChangeTable {
+	return repository.currentRates
+}
+
+func (repository Repository) ClearAllTable() error {
+	repository.currentRates = nil
+	return nil
+}
