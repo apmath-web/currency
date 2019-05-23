916a8d8a66a552f50867ff30e47e035c541fb576
sashachernyshova <st056039@student.spbu.ru>
2019-05-23 19:14:06 +0300

    add routing

--- Application/v1/routing/router.go
+++ Application/v1/routing/router.go
@@ -1,11 +1,15 @@
 package routing
 
 import (
+	"github.com/apmath-web/currency/Application/v1/actions"
 	"github.com/gin-gonic/gin"
 )
 
 func GenRouter() *gin.Engine {
 	router := gin.Default()
-	_ = router.Group("/v1")
+	v1 := router.Group("/v1")
+	{
+		v1.POST("/", actions.CurrencyHandler)
+	}
 	return router
 }
