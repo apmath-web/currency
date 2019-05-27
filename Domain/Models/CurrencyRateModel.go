f89fc28370b923271fb0c645cd2babab8b087bb5
Anatoly Efimov <efimo.tolik@gmail.com>
2019-05-27 18:49:23 +0300

    Крректрирвоание доменнх моделей

--- Domain/Models/CurrencyRateModel.go
+++ Domain/Models/CurrencyRateModel.go
@@ -1,17 +1,13 @@
 package domainModels
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"net/http"
-
 	"github.com/apmath-web/currency/Domain"
 )
 
 type CurrencyRateModel struct {
 	basedCurrency  Domain.Currency
 	wantedCurrency Domain.Currency
+	Rate           float64
 }
 
 func (i CurrencyRateModel) GetBasedCurrency() Domain.Currency {
@@ -23,42 +19,13 @@ func (i CurrencyRateModel) GetWantedCurrency() Domain.Currency {
 }
 
 func (i CurrencyRateModel) GetRate() float64 {
-	url := "https://api.exchangeratesapi.io/latest?symbols=" + i.wantedCurrency.GetName() + "&base=" + i.basedCurrency.GetName()
-	res, err := http.Get(url)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-
-	type ratesT struct {
-		rate float64
-	}
-
-	type dataT struct {
-		Base  string `json:"base"`
-		Rates ratesT `json:"rates`
-	}
-
-	data := dataT{
-		i.basedCurrency.GetName(),
-		ratesT{},
-	}
-
-	errOfUnmurshal := json.Unmarshal(body, &data)
-	if errOfUnmurshal != nil {
-		log.Fatal(errOfUnmurshal)
-	}
-
-	return data.Rates.rate
+	return i.Rate
 }
 
-func GenCurrencyRateDomainModel(bCurrency Domain.Currency, wCurrency Domain.Currency) CurrencyRateModel {
+func GenCurrencyRateDomainModel(bCurrency Domain.Currency, wCurrency Domain.Currency, rate float64) CurrencyRateModel {
 	return CurrencyRateModel{
 		bCurrency,
 		wCurrency,
+		rate,
 	}
 }
