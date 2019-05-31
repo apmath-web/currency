857ba79d9bd712bfd5cc621b39a0909a84b7aa16
KostyaLukyanchikov <mrwithoutnickname@gmail.com>
2019-05-31 13:06:17 +0300

    fix

--- Domain/serviceInterface.go
+++ Domain/serviceInterface.go
@@ -1,11 +1,13 @@
 package Domain
 
+import domainModels "github.com/apmath-web/currency/Domain/Models"
+
 type UpdaterTable interface {
 	Update(currentTable ChangeTable) (ChangeTable, error)
 }
 
-type CurrencyChanger interface {
-	Change(wantToChange CurrencyChange, wantedCurrency Currency) CurrencyChange
+type CurrencyChangerInterface interface {
+	Change(curCurrency domainModels.CurrencyModel, wanCurrency domainModels.CurrencyModel, Amount float64) float64
 }
 
 type ChangeTableServiceInterface interface {
--- Domain/services/currencychanger.go
+++ Domain/services/currencychanger.go
@@ -2,19 +2,20 @@ package services
 
 import (
 	"github.com/apmath-web/currency/Domain"
+	"github.com/apmath-web/currency/Domain/Models"
 )
 
 type CurrencyChangerService struct {
 	changeTable Domain.ChangeTableRepositoryInterface
 }
 
-func Create(changeTable Domain.ChangeTableRepositoryInterface) *CurrencyChangerService{
-	var currencyChangerService = new(CurrencyChangerService)
-	currencyChangerService.changeTable = changeTable
-	return currencyChangerService
+func Create(changeTable Domain.ChangeTableRepositoryInterface) Domain.CurrencyChangerInterface {
+	service := new(CurrencyChangerService)
+	service.changeTable = changeTable
+	return service
 }
 
-func (ccs* CurrencyChangerService) CurrencyChanger(baseCurrency string, wantedCurrency string, amount float64) float64{
-	rate := ccs.changeTable.GetRate(baseCurrency, wantedCurrency).GetRate()
-	return amount * rate
+func (ccs *CurrencyChangerService) Change(curCurrency domainModels.CurrencyModel, wanCurrency domainModels.CurrencyModel, Amount float64) float64 {
+	rate := ccs.changeTable.GetRate(curCurrency.GetName(), wanCurrency.GetName()).GetRate()
+	return Amount * rate
 }
