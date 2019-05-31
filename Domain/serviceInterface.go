9a8b049a15c91eb48c1f860b73c7930e43043259
KostyaLukyanchikov <mrwithoutnickname@gmail.com>
2019-05-31 13:06:17 +0300

    fix

--- Domain/serviceInterface.go
+++ Domain/serviceInterface.go
@@ -1,11 +1,13 @@
 package Domain
 
+import domainModels "github.com/apmath-web/currency/Domain/Models"
+
 type UpdaterTable interface {
 	Update() (ChangeTable, error)
 }
 
-type CurrencyChanger interface {
-	Change(curCurrency Currency, wanCurrency Currency, Amount int) CurrencyChange
+type CurrencyChangerInterface interface {
+	Change(curCurrency domainModels.CurrencyModel, wanCurrency domainModels.CurrencyModel, Amount float64) float64
 }
 
 type ChangeTableServiceInterface interface {
