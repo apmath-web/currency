5152e80b33faa41f4b242d48f7626637759384f5
sashachernyshova <st056039@student.spbu.ru>
2019-06-02 16:02:09 +0300

    changes RepositoryInterface Get

--- Domain/repositoriesInterface.go
+++ Domain/repositoriesInterface.go
@@ -2,5 +2,5 @@ package Domain
 
 type RepositoryInterface interface {
 	Set(data CurrencyRateInterface) error
-	Get(from string, to string) RateInterface
+	Get(from Currency, to Currency) RateInterface
 }
