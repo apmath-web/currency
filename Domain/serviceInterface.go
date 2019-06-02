3172bcf48caacc6eace93e0c480bcbe848c63ac3
sashachernyshova <st056039@student.spbu.ru>
2019-06-02 16:03:20 +0300

    changes ExchangeInterface type of what you get from Exchange

--- Domain/serviceInterface.go
+++ Domain/serviceInterface.go
@@ -6,12 +6,15 @@ type UpdaterInterface interface {
 
 type ExchangerInterface interface {
 	Exchange(data CurrencyChangeInterface) AmountInterface
+<<<<<<< HEAD
 }
 
 type FetchRateInterface interface {
 	GetBaseCurrency() string
 	GetWantedCurrency() string
 	GetRate() float64
+=======
+>>>>>>> changes ExchangeInterface type of what you get from Exchange
 }
 
 type FetcherInterface interface {
