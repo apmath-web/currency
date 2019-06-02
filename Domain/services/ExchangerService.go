fa4472d46571dbe176d24a59b871af1b5a583250
sashachernyshova <st056039@student.spbu.ru>
2019-06-02 23:09:16 +0300

    add Exchanger
    change multiply logic

--- Domain/services/ExchangerService.go
+++ Domain/services/ExchangerService.go
@@ -2,15 +2,14 @@ package services
 
 import (
 	"github.com/apmath-web/currency/Domain"
+	"github.com/apmath-web/currency/Domain/Models"
 )
 
 type Exchanger struct {
-	rates     Domain.RepositoryInterface
-	exchanger Domain.ExchangerInterface
+	repository Domain.RepositoryInterface
 }
 
-func (exchanger *Exchanger) Exchange(data Domain.CurrencyChangeInterface) Domain.AmountInterface {
-	//rate := exchanger.changeTable.GetRate(curCurrency.GetName(), wanCurrency.GetName()).GetRate()
-	rate:=rates
-	return data.GetAmount() * rate
+func (instance *Exchanger) Exchange(data Domain.CurrencyChangeInterface) Domain.AmountInterface {
+	rate := instance.repository.Get(data.GetWantedCurrency(), data.GetBaseCurrency())
+	return domainModels.GenAmount(int(float64(data.GetAmount()) * rate.GetRate()))
 }
