a5c8192e1fbdc9be701c5ba4b2b119b6a3ee68f8
Anatoly <efimo.tolik@gmail.com>
2019-06-03 19:13:22 +0300

    fix comutation of rate

--- Domain/services/UpdaterService.go
+++ Domain/services/UpdaterService.go
@@ -11,12 +11,19 @@ type Updater struct {
 }
 
 func (instance *Updater) Update() error {
+	instance.repository.Set(
+		domainModels.GenCurrency("RUB"),
+		domainModels.GenCurrency("RUB"),
+		domainModels.GenRate(1.0))
+
 	rates := instance.fetcher.FetchAll()
 	for _, currency1 := range rates {
 		for _, currency2 := range rates {
-			_ = instance.repository.Set(domainModels.GenCurrency(currency1.GetCurrency()),
+			instance.repository.Set(
+				domainModels.GenCurrency(currency1.GetCurrency()),
 				domainModels.GenCurrency(currency2.GetCurrency()),
-				domainModels.GenRate(1/(1/currency1.GetRate())/(1/currency2.GetRate())))
+				domainModels.GenRate(currency1.GetRate()/currency2.GetRate()))
 		}
 	}
+	return nil
 }
