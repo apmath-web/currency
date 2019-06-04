42e5d1a73faa28c819a962cd6f3bc044ca9909f0
Anatoly Efimov <anatolyefimov@yandex-team.ru>
2019-06-04 23:32:01 +0300

    resolve 2

--- Domain/Models/CurrencyChangeModel.go
+++ Domain/Models/CurrencyChangeModel.go
@@ -4,19 +4,11 @@ import "github.com/apmath-web/currency/Domain"
 
 type CurrencyChange struct {
 	amount         int
-<<<<<<< HEAD
 	baseCurrency   string
 	wantedCurrency string
 }
 
 func (i *CurrencyChange) GetBaseCurrency() string {
-=======
-	baseCurrency   Domain.CurrencyInterface
-	wantedCurrency Domain.CurrencyInterface
-}
-
-func (i *CurrencyChange) GetBaseCurrency() Domain.CurrencyInterface {
->>>>>>> dev
 	return i.baseCurrency
 }
 
@@ -24,18 +16,10 @@ func (i *CurrencyChange) GetAmount() int {
 	return i.amount
 }
 
-<<<<<<< HEAD
 func (i *CurrencyChange) GetWantedCurrency() string {
 	return i.wantedCurrency
 }
 
 func GenCurrencyChange(baseCurrency string, wantedCurrency string, amount int) Domain.CurrencyChangeInterface {
-=======
-func (i *CurrencyChange) GetWantedCurrency() Domain.CurrencyInterface {
-	return i.wantedCurrency
-}
-
-func GenCurrencyChange(baseCurrency Domain.CurrencyInterface, wantedCurrency Domain.CurrencyInterface, amount int) Domain.CurrencyChangeInterface {
->>>>>>> dev
 	return &CurrencyChange{amount, baseCurrency, wantedCurrency}
 }
